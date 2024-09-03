01e09816abbebf6f0342d80d890669076658af2c
xixiaolu <2946679089@qq.com>
2024-09-03 12:18:31 +0800

    新增vue项目

---
+++ demo/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
